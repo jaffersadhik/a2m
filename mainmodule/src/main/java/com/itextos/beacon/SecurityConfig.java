48886036871e6f329465cbf3578ff8716c1ab7eb
DESKTOP-ANR3VAM\Jaffer <jsadhik009@gmail.com>
2025-09-26 10:43:53 +0530

    jj

---
+++ mainmodule/src/main/java/com/itextos/beacon/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.itextos.beacon;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().permitAll()  // Allow all requests
+            )
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+            .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+            .build();
+    }
+}
