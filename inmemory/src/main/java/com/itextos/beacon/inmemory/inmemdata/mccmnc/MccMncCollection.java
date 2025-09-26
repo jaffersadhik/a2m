8a4ae95253971b59a372b68e0face76a237e490b
DESKTOP-ANR3VAM\Jaffer <jsadhik009@gmail.com>
2025-09-26 20:52:15 +0530

    jj

--- inmemory/src/main/java/com/itextos/beacon/inmemory/inmemdata/mccmnc/MccMncCollection.java
+++ inmemory/src/main/java/com/itextos/beacon/inmemory/inmemdata/mccmnc/MccMncCollection.java
@@ -17,7 +17,7 @@ public class MccMncCollection
     // Configuration constants - can be externalized to properties
     private static final int BATCH_SIZE = 100;
     private static final int YIELD_FREQUENCY = 50;
-    private static final long MAX_BATCH_TIME_MS = 2000; // Reduced from 5s to 2s
+    private static final long MAX_BATCH_TIME_MS = 500; // Reduced from 5s to 2s
     private static final long THROTTLE_DELAY_MS = 50;   // Small delay between batches
     private static final double CPU_THRESHOLD = 80.0;
     private static final int MAX_ROWS_BEFORE_YIELD = 1000;
